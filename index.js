8213b6403994fb3d8b232dcbb7b10018b49c7bfd
Alexis <alexis_keeter@yahoo.com>
2023-04-26 16:02:21 -0700

    new code in index.js

--- index.js
+++ index.js
@@ -12,102 +12,88 @@ const Engineer = require('./lib/Engineer');
 const newTeamMember = [];
 
 // questions
-const mainQs = [
-    {
-        type: 'input',
-        name: "name",
-        message: "What is your name?",
-    },
-    {
-        type: 'input',
-        name: 'id',
-        message: 'What is your id?',
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: "What is your email?",
-    },
-    {
-        type: 'list',
-        name: 'role',
-        message: 'What is your role?',
-        choices: ['Engineer', 'Intern', 'Manager', 'Exit'],
-    }
-    ]
-
-const employeeType = responses.role;
-
-// if manager is selected
-
-if (employeeType === 'Manager') {
-    const managerQs = [
-        {
-            type: 'input',
-            name: 'officeNumber',
-            message: 'What is the team managers office number?',
-        },
-    ]
-    const newManager = new Manager(
-        responses.name,
-        responses.id,
-        responses.email,
-        responses.officeNumber
-    );
-    newTeamMemeber.push(newManager);
-}
-
-// if engineer is selected
-if (employeeType === 'Engineer') {
-    const engineerQs = [
-        {
-            type: 'input',
-            name: 'github',
-            message: 'What is your github?',
-        },
-    ]
-    const newEngineer = new Engineer(
-        responses.name,
-        responses.id,
-        responses.email,
-        responses.github
-    );
-    newTeamMember.push(newEngineer)
-}
-
-// if intern is selected
-if (employeeType === 'Intern') {
-    const internQs = [
+const mainQs = {
+    initial: [ {
+          type: 'input',
+          name: "name",
+          message: "What is your name?",
+      },
+      {
+          type: 'input',
+          name: 'id',
+          message: 'What is your id?',
+      },
+      {
+          type: 'input',
+          name: 'email',
+          message: "What is your email?",
+      },
+      {
+          type: 'list',
+          name: 'role',
+          message: 'What is your role?',
+          choices: ['Engineer', 'Intern', 'Manager', 'Exit'],
+      }],
+        engineer = [
+          {
+              type: 'input',
+              name: 'github',
+              message: 'What is your github name?',
+          }, 
+          {
+              type: 'input',
+              name: 'quit',
+              message: 'do you want to quit',
+              choices: ['true', 'false'],
+          }
+      ],
+      intern = [
         {
             type: 'input',
             name: 'school',
             message: 'What is your school name?',
-        },
-    ]
-    const newIntern = new Intern (
-        responses.name,
-        responses.id,
-        responses.email,
-        responses.school
-);
-newTeamMemeber.push(newIntern);
-};
+        }, 
+        {
+            type: 'input',
+            name: 'quit',
+            message: 'do you want to quit',
+            choices: ['true', 'false'],
+        }
+    ],
+    manager = [
+        {
+            type: 'input',
+            name: 'officeNumber',
+            message: 'What is the team managers office number?',
+        }, 
+        {
+            type: 'input',
+            name: 'quit',
+            message: 'do you want to quit',
+            choices: ['true', 'false'],
+        }
+    ],
+  }
 
+  function prompt(questions) {
+    inquirer.prompt(questions).then( response => {
+  if (response.role === "Engineer") {
+   //all of the code to make it work depending in the role
+  }
+  if (response.role === "Intern") {
+   //all of the code to make it work depending in the role
+  }
+  if (response.role === "Manager") {
+    //all of the code to make it work depending in the role
+   }
+  if(response.quit === "true") {
+  //here you write the file and return;
+  } else {
+     prompt(questions) // starts the questionnaire from the begging to add another employee. 
+   }
+  })
+  }
 
-//function to write html file
-function writeToFile(fileName, responses) {
-    return fs.fstat.writeFileSync(path.json(process.cwd(), fileName), responses);
-};
- 
-
-//function to initialize app
-function init() {
-
-    inquirer.prompt(mainQs)
-    .then((responses) => writeFile('employees.html', template(responses)))
-    .then(() => console.log('Sucessfully wrote to employees.html!'))
-    .catch((err) => console.error(err));
-};
-
-// Function call to initialize app
-init();
+  function main() {
+    prompt(mainQs["initial"]);
+   }
