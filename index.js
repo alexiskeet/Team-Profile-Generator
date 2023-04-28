6b839f4e3628704c838c5221e92512a886815d87
Alexis <alexis_keeter@yahoo.com>
2023-04-27 17:50:07 -0700

    able to run prompts

--- index.js
+++ index.js
@@ -13,88 +13,94 @@ const newTeamMember = [];
 
 // questions
 const mainQs = {
-    initial: [ {
-          type: 'input',
-          name: "name",
-          message: "What is your name?",
-      },
-      {
-          type: 'input',
-          name: 'id',
-          message: 'What is your id?',
-      },
-      {
-          type: 'input',
-          name: 'email',
-          message: "What is your email?",
-      },
-      {
-          type: 'list',
-          name: 'role',
-          message: 'What is your role?',
-          choices: ['Engineer', 'Intern', 'Manager', 'Exit'],
-      }],
-        engineer : [
-          {
-              type: 'input',
-              name: 'github',
-              message: 'What is your github name?',
-          }, 
-          {
-              type: 'input',
-              name: 'quit',
-              message: 'do you want to quit',
-              choices: ['true', 'false'],
-          }
-      ],
-      intern : [
+    initial: [{
+        type: 'input',
+        name: "name",
+        message: "What is your name?",
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: 'What is your id?',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: "What is your email?",
+    },
+    {
+        type: 'list',
+        name: 'role',
+        message: 'What is your role?',
+        choices: ['Engineer', 'Intern', 'Manager', 'Exit'],
+    }],
+    engineer: [
+        {
+            type: 'input',
+            name: 'github',
+            message: 'What is your github name?',
+        },
+        {
+            type: 'list',
+            name: 'quit',
+            message: 'do you want to quit',
+            choices: ['true', 'false'],
+        }
+    ],
+    intern: [
         {
             type: 'input',
             name: 'school',
             message: 'What is your school name?',
-        }, 
+        },
         {
-            type: 'input',
+            type: 'list',
             name: 'quit',
             message: 'do you want to quit',
             choices: ['true', 'false'],
         }
     ],
-    manager : [
+    manager: [
         {
             type: 'input',
             name: 'officeNumber',
             message: 'What is the team managers office number?',
-        }, 
+        },
         {
-            type: 'input',
+            type: 'list',
             name: 'quit',
             message: 'do you want to quit',
             choices: ['true', 'false'],
         }
     ],
-  }
+}
 
-  function prompt(questions) {
-    inquirer.prompt(questions).then( response => {
-  if (response.role === "Engineer") {
-   //all of the code to make it work depending in the role
-   
-  }
-  if (response.role === "Intern") {
-   //all of the code to make it work depending in the role
-  }
-  if (response.role === "Manager") {
-    //all of the code to make it work depending in the role
-   }
-  if(response.quit === "true") {
-  //here you write the file and return;
-  } else {
-     prompt(questions) // starts the questionnaire from the begging to add another employee. 
-   }
-  })
-  }
 
-  function main() {
+function prompt(questions) {
+    inquirer.prompt(questions).then(response => {
+        if (response.role === "Engineer") {
+            //all of the code to make it work depending in the role
+            prompt(mainQs["engineer"])
+        }
+        if (response.role === "Intern") {
+            //all of the code to make it work depending in the role
+            prompt(mainQs["intern"]);
+        }
+        if (response.role === "Manager") {
+            //all of the code to make it work depending in the role
+            prompt(mainQs["manager"]);
+        }
+        if (response.quit === "true") {
+            //here you write the file and return;
+            writeFile('employeeHtml.md', template(responses))
+        } else {
+            prompt(questions) // starts the questionnaire from the begging to add another employee. 
+        }
+    })
+};
+
+function main() {
     prompt(mainQs["initial"]);
-   }
+};
+
+main();
